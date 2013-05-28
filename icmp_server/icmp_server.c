9bdd8bfc4a967e87e8bfb44fad0dc599e7153277
mhk <mathias@mathiashabluetzel.ch>
2013-05-28 10:28:28 +0200

    added flag for interface, documented code

--- icmp_server/icmp_server.c
+++ icmp_server/icmp_server.c
@@ -4,7 +4,7 @@
  * gcc -o icmp_server icmp_server.c
  *
  * how to execute:
- * sudo ./icmp_server
+ * sudo ./icmp_server -i <INTERFACE>
  */
 
 #define __USE_MISC
@@ -26,6 +26,8 @@
 #include <net/if.h>
 #include <errno.h>
 #include <netpacket/packet.h>
+
+#include "debug.h"
 
 #define MAX_MTU 1500 /* random */
 
@@ -61,19 +63,42 @@ rfc1701_cksum(uint16_t *addr, unsigned short int len)
     return ((uint16_t) ~sum);
 }
 
+static int
+is_arg_missing(const int option, const char* optarg, char* argv[])
+{
+  if (0 == strncmp("-", optarg, 1)) {
+    fprintf(stderr, "%s: option requires an argument -- '%c'\n", argv[0], option);
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 int
 main(int argc, char **argv)
 {
     /* ICMP variables */
-    int ret = 0;
-    int sock_eth;
-    struct ether_header *eth_hdr_in, *eth_hdr_out;
-    struct ip *ip_hdr_in, *ip_hdr_out;
+    int c, ret = 0;
+    char *iface = "eth0";
+    int sock_eth = 0;
     char buf_in[MAX_MTU], buf_out[MAX_MTU];
-    struct icmp *icmp_hdr_in, *icmp_hdr_out;
-    int ip_len, icmp_len, icmp_data_len;
+    struct ether_header *eth_hdr_in, *eth_hdr_out = NULL;
+    struct ip *ip_hdr_in, *ip_hdr_out = NULL;
+    struct icmp *icmp_hdr_in, *icmp_hdr_out = NULL;
+    int ip_len, icmp_len, icmp_data_len = 0;
     struct ifreq *ifinfo = NULL;
     struct sockaddr_ll sockinfo;
+
+    while ((c = getopt(argc, argv, "i:")) != -1 ) {
+      switch (c) {
+        case 'i':
+          is_arg_missing(c, optarg, argv);
+          iface = optarg;
+          break;
+        default:
+          exit(EXIT_FAILURE);
+      }
+    }
 
     if ((sock_eth = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
         perror("socket");
@@ -83,7 +108,7 @@ main(int argc, char **argv)
     /* get interface index */ 
     ifinfo = malloc(sizeof(struct ifreq));
     memset(ifinfo, 0, sizeof(struct ifreq));
-    strncpy(ifinfo->ifr_name, "p3p1", IFNAMSIZ); /* @FIXIT sorry, hardcoded */
+    strncpy(ifinfo->ifr_name, iface, IFNAMSIZ);
     if ( -1 == ioctl(sock_eth, SIOCGIFINDEX, ifinfo)) {
       perror("can't get interface index");
       exit(EXIT_FAILURE);
@@ -105,28 +130,34 @@ main(int argc, char **argv)
           exit(EXIT_FAILURE);
       }
     }
-    
+
+    /* our input is a raw ethernet frame */    
     eth_hdr_in  = (struct ether_header *)buf_in;
     eth_hdr_out  = (struct ether_header *)buf_out;
+    /* after the ethernet header comes the ipv4 header */
     ip_hdr_in   = (struct ip *)(buf_in + sizeof(struct ether_header));
-    icmp_hdr_in = (struct icmp *)((unsigned char *)ip_hdr_in +
-                                                   sizeof(struct ip));
-
     ip_hdr_out   = (struct ip *)(buf_out + sizeof(struct ether_header));
+    /* and finally after the ipv4 header comes the icmp packet */
+    icmp_hdr_in = (struct icmp *)((unsigned char *)ip_hdr_in + sizeof(struct ip));
     icmp_hdr_out = (struct icmp *)((unsigned char *)ip_hdr_out + sizeof(struct ip));
 
     while(1) { /* ICMP processing loop */
 
+        /* reading from the raw socket into buf_in */
         if ((ret = recvfrom(sock_eth, buf_in, sizeof(buf_in), 0, NULL, NULL)) < 1) {
             perror("recv");
             exit(1);
         }
-        
+       
+        /* if we received an ICMP and ICMP-Echo packet we continue */ 
         if (ip_hdr_in->ip_p == IPPROTO_ICMP) {
             if (icmp_hdr_in->icmp_type == ICMP_ECHO) {
-                /* set MAC header */
+                debug("ICMP_ECHO request.\n");
+
+                /* we switch the MAC fields for sending it back */
                 memcpy(eth_hdr_out->ether_shost, eth_hdr_in->ether_dhost, 6);
                 memcpy(eth_hdr_out->ether_dhost, eth_hdr_in->ether_shost, 6);
+                /* it is an ipv4 packet we want to send back */
                 eth_hdr_out->ether_type = htons(ETH_P_IP);
 
                 /* Prepare outgoing IP header. */
@@ -134,18 +165,23 @@ main(int argc, char **argv)
                 ip_hdr_out->ip_hl         = ip_hdr_in->ip_hl;
                 ip_hdr_out->ip_tos        = 0;
                 ip_hdr_out->ip_len        = ip_hdr_in->ip_len;
-                ip_hdr_out->ip_id         = ip_hdr_in->ip_id + 5321;
+                ip_hdr_out->ip_id         = ip_hdr_in->ip_id;
+                /* we set the IP Don't Fragment mask */
                 ip_hdr_out->ip_off        = htons(IP_DF);
                 ip_hdr_out->ip_ttl        = 64;
                 ip_hdr_out->ip_p          = IPPROTO_ICMP;
                 ip_hdr_out->ip_sum        = 0;
+                /* we switch the dst and src ipv4 */
                 ip_hdr_out->ip_src.s_addr = ip_hdr_in->ip_dst.s_addr;
                 ip_hdr_out->ip_dst.s_addr = ip_hdr_in->ip_src.s_addr;
-
+                /* calculate the ip header checksum 
+                 * bear in mind that ip_hl is a double-word and the parameter
+                 * wants a byte number (8bits)
+                 */
                 ip_hdr_out->ip_sum = rfc1701_cksum((unsigned short *)ip_hdr_out,
                                               ip_hdr_out->ip_hl * 4);
 
-                printf("0x%02x 0x%02x\n", htons(ip_hdr_in->ip_sum), htons(ip_hdr_out->ip_sum));
+                debug("0x%02x 0x%02x\n", htons(ip_hdr_in->ip_sum), htons(ip_hdr_out->ip_sum));
 
                 /* Prepare outgoing ICMP header. */
                 icmp_hdr_out->icmp_type  = 0;
@@ -156,16 +192,16 @@ main(int argc, char **argv)
                 
                 ip_len = ntohs(ip_hdr_out->ip_len);
                 icmp_len = ip_len - sizeof(struct ip);
-                icmp_data_len =  icmp_len - sizeof(struct icmphdr);
+                icmp_data_len = icmp_len - sizeof(struct icmphdr);
 
-                printf("ICMP_ECHO request.\n");
-
+                /* we just need to echo the incoming data */
                 memcpy(icmp_hdr_out->icmp_data, icmp_hdr_in->icmp_data,
                        icmp_data_len);
-
+                /* finally a little checksum */
                 icmp_hdr_out->icmp_cksum =
                     rfc1701_cksum((unsigned short *)icmp_hdr_out, icmp_len);
 
+                /* send it on the wire */
                 ret = sendto(sock_eth, buf_out, ret, 0,
                                              (struct sockaddr *)&sockinfo,
                                              sizeof(sockinfo));
@@ -179,4 +215,3 @@ main(int argc, char **argv)
     close(sock_eth);
 } 
 
-/* __END__ */
